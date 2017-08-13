384adcb0110d59e58d6d706fc054bb9bb3af22b1
Hardik Shah <hardiks@hardiks-m01.vmware.com>
2017-08-13 14:50:18 -0700

    added all protein sequences

--- bio-protine.py
+++ bio-protine.py
@@ -4,21 +4,28 @@ from flask import request
 app = Flask(__name__)
 
 
+dna_seq = "attttaagttgcctgtcatttga"
+f = open("NC_007346")
+s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+if s.find(dna_seq) != -1:
+	print str(s.find(dna_seq)+1)
+else:
+	print "No matching protein subsequence found!"
 
-@app.route('/', methods=['GET', 'POST'])
-def get_ans():
-	if request.method == "POST":
-		obj = request.get_json()
-		dna_seq = obj['dna_seq'];
-		f = open("NC_000852.txt")
-		s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-		if s.find(dna_seq) != -1:
-			return str(s.find(dna_seq)+1)
-		else:
-			return "No matching protein subsequence found!"
+# @app.route('/', methods=['GET', 'POST'])
+# def get_ans():
+# 	if request.method == "POST":
+# 		obj = request.get_json()
+# 		dna_seq = obj['dna_seq'];
+# 		f = open("NC_000852.txt")
+# 		s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+# 		if s.find(dna_seq) != -1:
+# 			return str(s.find(dna_seq)+1)
+# 		else:
+# 			return "No matching protein subsequence found!"
 		
 
 
-if __name__ == '__main__':
-	app.run()
+# if __name__ == '__main__':
+# 	app.run()
 
